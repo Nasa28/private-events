04c731a7b952982b5360408a1b31870fb143bdff
Nasa28 <kaluchinasaagu@gmail.com>
2021-03-30 00:23:55 +0100

    add login and logout methods to SessionsHelper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to private events App!"
       redirect_to new_sessions_path
     else
