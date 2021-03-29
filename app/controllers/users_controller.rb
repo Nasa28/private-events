ba58e7153436a77840daaa31f14ba73035c85986
Nasa28 <kaluchinasaagu@gmail.com>
2021-03-29 22:53:19 +0100

    generate sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to private events App!"
       redirect_to new_sessions_path
     else
       render :new
@@ -13,6 +14,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'pages/home'
   resources :users, only: [:new, :create, :show]
 end
