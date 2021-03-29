f49280821e944b9bd2ef99b31e7a656601475962
Nasa28 <kaluchinasaagu@gmail.com>
2021-03-29 21:44:53 +0100

    Set up routes for the controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_sessions_path
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
