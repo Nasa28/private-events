8bbe2a6cdfe7d46556acd6ec2f670569067098f8
Nasa28 <kaluchinasaagu@gmail.com>
2021-03-31 22:46:14 +0100

    build and migrate Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_201033) do
+ActiveRecord::Schema.define(version: 2021_03_31_213825) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "description"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
