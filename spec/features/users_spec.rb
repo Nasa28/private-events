d3ccef2e798532038cdbb38c7ab691e950e3efac
Nasa28 <kaluchinasaagu@gmail.com>
2021-04-09 23:50:11 +0100

    add capybara test to users

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'create new user' do
+    scenario 'should be successful' do
+      visit new_user_path
+      within('form') do
+        fill_in 'user_username', with: 'chinasa', visible: true
+      end
+      click_button 'Sign Up'
+      expect(current_path).to eq(events_path)
+    end
+
+    scenario 'should be successful' do
+      visit new_user_path
+      within('form') do
+        fill_in 'user_username', with: ''
+      end
+      click_button 'Sign Up'
+      expect(current_path).not_to eq(events_path)
+    end
+end
 end
