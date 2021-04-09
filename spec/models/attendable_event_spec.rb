b5c0724542550bdbd4eef37e26c26fbb30c75bc0
Nasa28 <kaluchinasaagu@gmail.com>
2021-04-09 23:21:56 +0100

    add test for attendable event model

---
+++ spec/models/attendable_event_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe AttendableEvent, type: :model do
+  
+  context 'attendable events test' do
+    it 'belongs to event attendee' do
+      first = AttendableEvent.reflect_on_association(:event_attendee).macro
+      expect(first).to eq(:belongs_to)
+    end
+
+    it 'belongs to  attended event' do
+      first = AttendableEvent.reflect_on_association(:attended_event).macro
+      expect(first).to eq(:belongs_to)
+    end
+  end
+end
