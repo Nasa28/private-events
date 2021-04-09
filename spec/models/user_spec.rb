d79b2a0e7c0a8b33d35fb0efc414811d3e8268ca
Nasa28 <kaluchinasaagu@gmail.com>
2021-04-09 21:28:07 +0100

    add validation test for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validation test' do 
+    it 'ensures username presence' do
+      user = User.new(username: " ").save
+      expect(user).to eq(false)
+    end
+    
+    it 'should save successfully' do
+      user = User.new(username: 'chinasa').save
+      expect(user).to eq(true)
+    end
+  end
 end
