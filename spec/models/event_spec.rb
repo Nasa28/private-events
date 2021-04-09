867f3a867d6abe06f85d2cd08f92638e76370937
Nasa28 <kaluchinasaagu@gmail.com>
2021-04-09 23:04:10 +0100

    add association test to event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -11,8 +11,23 @@ RSpec.describe Event, type: :model do
     event = Event.new(description: 'this is my post').save
     expect(event).to eq(false)
     end
-
   end
 
-  
+  context 'association test for Events' do
+    
+    it 'it should belong to event creator' do
+      first = Event.reflect_on_association(:creator).macro
+      expect(first).to eq(:belongs_to)
+    end
+
+    it 'it should belong to event creator' do
+      first = Event.reflect_on_association(:attendees).macro
+      expect(first).to eq(:has_many)
+    end
+
+    it 'it should belong to event creator' do
+      first = Event.reflect_on_association(:attendable_events).macro
+      expect(first).to eq(:has_many)
+    end
+  end
 end
