67ad3e7d57c642d35c7968639a46151677863847
Nasa28 <kaluchinasaagu@gmail.com>
2021-04-10 01:42:25 +0100

    modify user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,16 +17,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @upcoming_events = Event.upcoming.order('created_at ASC')
-    @past_event = Event.past.order('created_at DESC')
     @user = current_user
     @attending_events = current_user.attended_events
     @created_events_upcoming = current_user.created_events.upcoming.order('created_at DESC')
     @created_events_past = current_user.created_events.past.order('created_at DESC')
-    @attended_events_upcoming = current_user.attended_events.upcoming.order('created_at DESC')
+    @upcoming_attending_events = current_user.attended_events.upcoming.order('created_at DESC')
     @attended_events_past = current_user.attended_events.past.order('created_at DESC')
     @user = current_user
-    @my_events = current_user.created_events
   end
 
   private
